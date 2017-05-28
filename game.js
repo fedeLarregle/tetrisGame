e6e6f48de0a4c4d784b7d4127bf4be37dad69354
federico <fede.larregle@gmail.com>
2017-05-27 21:16:22 -0300

    Added 'collision' TODO collision with side walls

--- game.js
+++ game.js
@@ -3,12 +3,23 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function createMap(width, height) {
+	const map = [];
+	while ( height-- ) {
+		map.push(new Array(width).fill(0));
+	}
+	return map;
+}
+
+const map = createMap(12, 20);
+
 // This method will 'clear' the screen by re-filling it with black color
 // and drawing the figure_matrix on it's position all over again
 function draw() {
 	context.fillStyle = '#000';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
+	drawMatrix(map, {x: 0, y: 0});
 	drawMatrix(figure.matrix, figure.pos);
 }
 
@@ -24,8 +35,40 @@ function drawMatrix(matrix, offSet) {
 	});
 }
 
+// Checks if we collide with a '1' in our map 'matrix'
+function collide(map, figure, x, y) {
+	const toX = x;
+	const toY = y;
+	const m = figure.matrix;
+	const o = figure.pos;
+	for (let y = 0; y < m.length; ++y) {
+		for ( let x = 0; x < m[y].length; ++x ) {
+			if ( (m[y][x] !== 0) && ( (map[y + o.y + toY]) && (map[y + o.y + toY][x + o.x + toX])) !== 0 ) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+// Merges the map with the figures in it
+function merge(map, figure) {
+	figure.matrix.forEach( (row, y) => {
+		row.forEach((value, x) => {
+			if ( value !== 0 ) {
+				map[y + figure.pos.y][x + figure.pos.x] = value;
+			}
+		});
+	});
+}
+
 function update() {
-	figureDrop();
+	if ( collide(map, figure, 0, 1) ) {
+		merge(map, figure);
+		figure.pos.y = 0;
+	} else {
+		figureDrop();
+	}
 }
 
 // The most similar way to do a 'System.currentTimeMillis()' 
--- keyManager.js
+++ keyManager.js
@@ -4,7 +4,12 @@ const KEY_LEFT = 37;
 
 document.addEventListener('keydown', event => {
 	if ( event.keyCode === KEY_DOWN ) {
-		figureDrop();
+		if ( collide(map, figure, 0, 1) ) {
+			merge(map, figure);
+			figure.pos.y = 0;
+		} else {
+			figureDrop();
+		}
 	}
 	if ( event.keyCode === KEY_LEFT ) {
 		figureLeft();
